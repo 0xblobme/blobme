6118c3ddc468acfd8372e8dd9fd1ef6beb51bc8e
Jorah <169576348+0xjorah@users.noreply.github.com>
2024-05-29 22:01:33 +0800

    update check burner

--- packages/ui/src/components/check-burner/check-burner.tsx
+++ packages/ui/src/components/check-burner/check-burner.tsx
@@ -1,11 +1,16 @@
 "use client";
 
 import { useAtomValue } from "jotai";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { QRCodeSVG } from "qrcode.react";
+import { CopyIcon } from "lucide-react";
+import copy from "copy-to-clipboard";
+import { toast } from "sonner";
 
 import {
   AlertDialog,
   AlertDialogAction,
+  AlertDialogCancel,
   AlertDialogContent,
   AlertDialogFooter,
   AlertDialogHeader,
@@ -17,30 +22,70 @@ import { privateKeyAtom } from "@/store";
 export function CheckBurner() {
   const privateKey = useAtomValue(privateKeyAtom);
   const [open, setOpen] = useState(false);
-  const { generateWallet } = useMiner();
+  const [created, setCreated] = useState(false);
+  const { generateWallet, minerAddress } = useMiner();
 
-  const handleGoToGenerateWallet = () => {
+  const handleGoToGenerateWallet = useCallback(() => {
     generateWallet();
-    setOpen(false);
-  };
+    setCreated(true);
+  }, [generateWallet]);
+
+  const handleCopyAddress = useCallback(() => {
+    if (!minerAddress) return;
+
+    const success = copy(minerAddress);
+
+    if (success) {
+      toast.success("Copied address to clipboard");
+    }
+  }, [minerAddress]);
 
   useEffect(() => {
-    setOpen(!privateKey);
-  }, [privateKey]);
+    if (!created) {
+      setOpen(!privateKey);
+    }
+  }, [created, privateKey]);
 
   return (
     <AlertDialog open={open}>
-      <AlertDialogContent>
+      <AlertDialogContent className=" w-screen overflow-hidden">
         <AlertDialogHeader>
-          <AlertDialogTitle>Create new burner wallet?</AlertDialogTitle>
+          <AlertDialogTitle className="text-center">
+            {privateKey
+              ? "Send ETH to miner address"
+              : "Create new burner wallet?"}
+          </AlertDialogTitle>
         </AlertDialogHeader>
+        {minerAddress && (
+          <div className="flex flex-col items-center space-y-6 py-6">
+            <div className="bg-muted p-4 rounded-lg">
+              <QRCodeSVG value={minerAddress} size={256} />
+            </div>
+            <div
+              className="px-4 rounded-full bg-primary/10 py-2 break-all text-center"
+              onClick={handleCopyAddress}
+            >
+              <span className="text-sm leading-1">{minerAddress}</span>
+              <CopyIcon className="ml-2 w-4 h-4 inline-block" />
+            </div>
+          </div>
+        )}
         <AlertDialogFooter>
-          <AlertDialogAction
-            onClick={handleGoToGenerateWallet}
-            className="w-full rounded-full"
-          >
-            OK
-          </AlertDialogAction>
+          {privateKey ? (
+            <AlertDialogCancel
+              className="w-full rounded-full"
+              onClick={() => setOpen(false)}
+            >
+              Close
+            </AlertDialogCancel>
+          ) : (
+            <AlertDialogAction
+              onClick={handleGoToGenerateWallet}
+              className="w-full rounded-full"
+            >
+              OK
+            </AlertDialogAction>
+          )}
         </AlertDialogFooter>
       </AlertDialogContent>
     </AlertDialog>
