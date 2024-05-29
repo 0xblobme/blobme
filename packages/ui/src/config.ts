b846780f72fd6efc2a5c81c5eec697c5f178350f
Jorah <169576348+0xjorah@users.noreply.github.com>
2024-05-29 19:46:41 +0800

    update config

--- packages/ui/src/config.ts
+++ packages/ui/src/config.ts
@@ -16,7 +16,7 @@ export const SUPPORTED_CHAIN_IDS = [holesky.id, localhost.id];
 export type SUPPORTED_CHAIN_ID = (typeof SUPPORTED_CHAIN_IDS)[number];
 
 export const BLOBME_ADDRESSES: Record<SUPPORTED_CHAIN_ID, Address> = {
-  [holesky.id]: "0xdcb759134dc3ca0e39953e5b67d172c4fd6905c8",
+  [holesky.id]: "0x84E9a8007DD9a95c76E0693622522D36e9911Abd",
   [localhost.id]: "0x320ae55a5826c87165a8cf9c08621fDd48DdeCa8",
 };
 
